f432d3b3833e60fbb107abae73ba59757f70c4b8
Rajasekar Periyasamy <raja.periyasamy@concertidc.com>
2023-03-22 19:45:04 +0530

    env changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./globals/authConfig";
 
-console.log(process.env.environment)
+console.log(process.env.REACT_APP_DEV_APIBASEURI)
 // import './main.css'
 const msalInstance = new PublicClientApplication(msalConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
